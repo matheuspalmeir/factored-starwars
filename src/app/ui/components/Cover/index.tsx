4548bd2b9c92ee1098b86a4b5ab8fe69f061ae86
Matheus Palmeira <matheus_160497@hotmail.com>
2025-02-01 22:14:09 -0300

    [feature] Created Cover component to show movie covers

---
+++ src/app/ui/components/Cover/index.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
+type MovieCardProps = {
+  href: string;
+  alt: string;
+  linkTo: string;
+};
+
+const Cover = ({ href, alt, linkTo }: MovieCardProps) => {
+  return (
+    <Link href={linkTo} key={href}>
+      <div
+        className="flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white shadow-sm transition-transform duration-300 ease-in-out
+    hover:scale-110 active:scale-95 cursor-pointer"
+      >
+        <Image
+          className="h-120 w-full object-cover"
+          src={href}
+          width={250}
+          alt={alt}
+          height={375}
+        />
+      </div>
+    </Link>
+  );
+};
+
+export default Cover;
