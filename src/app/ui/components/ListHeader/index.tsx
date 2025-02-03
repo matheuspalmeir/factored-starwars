fd63a68facc8d18d5ab92eeab43948229b9e88d4
Matheus Palmeira <matheus_160497@hotmail.com>
2025-02-02 21:38:45 -0300

    [feature] Created the list components, Header and Row

---
+++ src/app/ui/components/ListHeader/index.tsx
@@ -0,0 +1,15 @@
+interface TableHeaderProps {
+  title: string;
+  description: string;
+}
+
+const ListHeader = ({ title, description }: TableHeaderProps) => {
+  return (
+    <div className="px-4 sm:px-0">
+      <h3 className="text-base/7 font-semibold text-white">{title}</h3>
+      <p className="mt-1 max-w-6xl text-sm/6 text-gray-300">{description}</p>
+    </div>
+  );
+};
+
+export default ListHeader;
---
+++ src/app/ui/components/Row/index.tsx
@@ -0,0 +1,17 @@
+interface RowProps {
+  title: string;
+  data: string | number;
+}
+
+const Row = ({ title, data }: RowProps) => {
+  return (
+    <div className="px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+      <dt className="text-sm/6 font-bold text-white">{title}</dt>
+      <dd className="mt-1 text-sm/6 text-gray-300 sm:col-span-1 sm:mt-0">
+        {data}
+      </dd>
+    </div>
+  );
+};
+
+export default Row;
