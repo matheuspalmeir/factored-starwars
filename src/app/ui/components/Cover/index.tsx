718b8647bdcb5d1acf1d86b8a8f90f7adb3c4db8
Matheus Palmeira <matheus_160497@hotmail.com>
2025-02-03 00:08:35 -0300

    [feature] Cover component allowing variants and added fallback image

--- src/app/ui/components/Cover/index.tsx
+++ src/app/ui/components/Cover/index.tsx
@@ -1,26 +1,41 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import React, { useState } from "react";
 
 type MovieCardProps = {
   href: string;
   alt: string;
   linkTo: string;
   classNames?: string;
+  fallbackSrc?: string;
+  variant?: "circle" | "card";
 };
 
-const Cover = ({ href, alt, linkTo, classNames }: MovieCardProps) => {
+const Cover = ({
+  href,
+  alt,
+  linkTo,
+  classNames,
+  fallbackSrc = "/fallback.png",
+  variant = "card",
+}: MovieCardProps) => {
+  const [imageSrc, setImageSrc] = useState(href);
   return (
     <Link href={linkTo} key={href}>
       <div
-        className="flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white shadow-sm transition-transform duration-300 ease-in-out
-    hover:scale-110 active:scale-95 cursor-pointer"
+        className={`relative overflow-hidden shadow-sm transition-transform duration-300 ease-in-out
+        hover:scale-110 active:scale-95 cursor-pointer ${
+          variant === "circle" ? "w-40 h-40 rounded-full" : "rounded-lg"
+        }`}
       >
         <Image
           className={classNames || "h-120 w-full object-cover"}
-          src={href}
+          src={imageSrc}
           width={250}
           alt={alt}
           height={375}
+          onError={() => setImageSrc(fallbackSrc)}
         />
       </div>
     </Link>
