69fd63ef571c0ab091b1494c45e10d45717e1245
Matheus Palmeira <matheus_160497@hotmail.com>
2025-02-03 00:07:38 -0300

    [feature] Created the Characters page

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,9 +1,32 @@
-const Characters = () => {
+import { CharactersResponse } from "../lib/types";
+import CharactersList from "../ui/components/CharactersList";
+import {
+  Character,
+  CharactersData,
+} from "../ui/components/CharactersList/types";
+
+export default async function Characters() {
+  const parseCharactersData = (
+    characters: CharactersResponse
+  ): CharactersData => {
+    return characters.results.map((c) => ({
+      uid: c.uid,
+      name: c.name,
+      url: c.url,
+    }));
+  };
+
+  const getSwapiCharacters = async (): Promise<Character[]> => {
+    const res = await fetch("http://localhost:3000/api/swapi/characters");
+    const data = (await res.json()) as CharactersResponse;
+    return parseCharactersData(data);
+  };
+
+  const data = await getSwapiCharacters();
+
   return (
-    <div>
-      <h1>Characters</h1>
+    <div className="min-h-screen mx-auto max-w-7xl p-4 sm:px-6 lg:px-8">
+      <CharactersList data={data} />
     </div>
   );
-};
-
-export default Characters;
+}
